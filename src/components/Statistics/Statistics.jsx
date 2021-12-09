d53d37ceacef2a2146c1952b5361323432b08c3a
Svitlana <87758266+Sv0707@users.noreply.github.com>
2021-12-09 18:05:57 +0200

    finish

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,8 +4,8 @@ import s from "./Statistics.module.css";
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return (
     <div>
-      <p className={s.value}>Good:{good}</p>
-      <p className={s.value}>Neutral:{neutral}</p>
+      <p className={s.value}>Good: {good}</p>
+      <p className={s.value}>Neutral: {neutral}</p>
       <p className={s.value}>Bad: {bad}</p>
       <p className={s.value}>Total: {total}</p>
       <p className={s.value}>Positive feedback: {positivePercentage}</p>
